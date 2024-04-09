ebb34ec0eba4b90466d58ddf8adf042e261f5c6d
202301207 <162132120+202301207@users.noreply.github.com>
2024-04-09 20:04:22 +0530

    Add files via upload

---
+++ CodeSnippetManager.h
@@ -0,0 +1,29 @@
+#ifndef CODE_SNIPPET_MANAGER_H
+#define CODE_SNIPPEY_MANAGER_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class CodeSnippetManager
+{
+private:
+    unordered_map<string, pair<string, string>> snippets;
+    string store_File;
+
+public:
+    CodeSnippetManager(string file = "xyz.txt");
+    int closestSnippet( string s1,string s2);
+    string findclosestSnippet( string input);
+    void saveSnippets();
+    void loadSnippets();
+    void addSnippet(string snippetname, string codeSnippet, string tagOfSnippet);
+    void getSnippet(string Snippetname);
+    void listSnippets();
+    void listTags();
+    void searchByTag(string tag);
+    void editSnippet(string Snippetname, string CodeOfSnippet, string SetOfTags);
+    void removeSnippet(string Snippet_name);
+    bool check_if_available(string name);
+};
+
+#endif
